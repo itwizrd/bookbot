6ece02ede22d064834c4ab2dd1b261581303372e
itwizrd <29903092+itwizrd@users.noreply.github.com>
2024-10-07 20:34:16 -0400

    reading book and printing to stdout

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as file:
+        return file.read()
+
+
+main()
