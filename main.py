d55886e0e756b878bd8e176780667cc3017eb778
itwizrd <29903092+itwizrd@users.noreply.github.com>
2024-10-13 13:33:59 -0400

    count characters

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
     text = get_book_text(book_path)
 #    print(text)
     num_words = get_num_words(text)
-    print(num_words)
+#    print(num_words)
+    num_char = get_num_char(text)
+    print(num_char)
 
 def get_book_text(path):
     with open(path) as file:
@@ -12,6 +14,15 @@ def get_book_text(path):
 def get_num_words(input_file):
     words = input_file.split()
     return len(words)
-    
+def get_num_char(text):
+    lower = text.lower()
+    char_dict = {}
+    for i in lower:
+        char_dict[i] = char_dict.get(i,0) + 1
+    return char_dict
+
+#    make a dictionary from lower string. key = letter, value = count?
+#    incriment each letter. for loop
+#    return dictionary
 
 main()
